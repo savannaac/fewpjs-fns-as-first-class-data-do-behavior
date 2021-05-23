7ac6bff782e246c74edf43f917f8ceb9440d2f6c
savanna chiang <savannaac@gmail.com>
2021-05-22 20:01:01 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let parsed = parseInt(string, 10);
+
+  if (parsed < 12) {
+    return "Good Morning";
+  } else if (parsed < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
